22b0687cfbf1908e51540e69ae05d8e6ec04a6e1
Ranjana121 <dixitrb5559@gmail.com>
2026-07-11 01:53:32 +0530

    Change loop condition from n > 0 to n != 0

--- Basic_Maths/ReverseNo.cpp
+++ Basic_Maths/ReverseNo.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>n;
     int reverse=0;
     int lastdigit=0;
-    while(n>0){
+    while(n!=0){
         lastdigit=n%10;
         reverse=reverse*10;
         reverse+=lastdigit;
